1d841877ef1678cbe8abd1b796e9ea03d50f511d
Jabbrowocky <mindbull3tz@gmail.com>
2017-11-22 23:01:02 -0600

    added pseudo animation/delay to menu printing with color randomization. Lookin pretty spiffy

--- LemonadeFoSale/LemonadeFoSale/UI.cs
+++ LemonadeFoSale/LemonadeFoSale/UI.cs
@@ -9,35 +9,59 @@ namespace LemonadeFoSale
     static class UI
     {
         //member variables
-        
-        static List<ConsoleColor> titleColors = new List<ConsoleColor>() {ConsoleColor.Green, ConsoleColor.Yellow, ConsoleColor.Cyan, ConsoleColor.Magenta, ConsoleColor.Red, ConsoleColor.Blue};
-        static Random rndColor;
+        static string userInput;
+        static List<ConsoleColor> titleColors = new List<ConsoleColor>() { ConsoleColor.Green, ConsoleColor.Yellow, ConsoleColor.Cyan, ConsoleColor.Magenta, ConsoleColor.Red, ConsoleColor.Blue };
+
         //constructor
 
 
         //member methods
         static public void DisplayTitleArt()
         {
+
             Console.ForegroundColor = TitleColorRandomizer();
-            Console.WriteLine("   _      _____ ___  ___ _____  _   _   ___  ______  _____  ");
+            Console.WriteLine("   _      _____ ___  ___ _____  _   _   ___  ______  _____   ");
             Console.WriteLine(@"  | |    |  ___||  \/  ||  _  || \ | | / _ \ |  _  \|  ___| ");
+            Console.ResetColor();
+            System.Threading.Thread.Sleep(500);
+            Console.ForegroundColor = TitleColorRandomizer();
             Console.WriteLine(@"  | |    | |__  | .  . || | | ||  \| |/ /_\ \| | | || |__   ");
+            Console.ResetColor();
+            System.Threading.Thread.Sleep(500);
+            Console.ForegroundColor = TitleColorRandomizer();
             Console.WriteLine(@"  | |    |  __| | |\/| || | | || . \ ||  _  || | | ||  __|  ");
-            Console.WriteLine(@"  | |____| |___ | |  | |\ \_/ /| |\  || | | || |/ / | |___  ");
-            Console.WriteLine(@"  \_____/\____/ \_|  |_/ \___/ \_| \_/\_| |_/|___/  \____/  ");
+            Console.WriteLine(@"  | |____| |___ | |  | |\ \_/ /| |\  || | | || |/  /| |___  ");
+            Console.ResetColor();
+            System.Threading.Thread.Sleep(500);
+            Console.ForegroundColor = TitleColorRandomizer();
+            Console.WriteLine(@"  \_____/\____/ \_|  |_/ \___/ \_| \_/\_| |_/|___./ \____/  ");
             Console.WriteLine(@"                                                          ");
             Console.WriteLine(@"                                                          ");
+            Console.ResetColor();
+            System.Threading.Thread.Sleep(500);
+            Console.ForegroundColor = TitleColorRandomizer();
             Console.WriteLine(@"                       _____  _____   ___   _   _ ______  ");
             Console.WriteLine(@"                      /  ___||_   _| / _ \ | \ | ||  _  \ ");
             Console.WriteLine(@"                      \  `--.  | |  / /_\ \|  \| || | | | ");
+            Console.ResetColor();
+            System.Threading.Thread.Sleep(500);
+            Console.ForegroundColor = TitleColorRandomizer();
             Console.WriteLine(@"                       `--.  \ | |  |  _  || . \ || | | | ");
             Console.WriteLine(@"                      /\__/  / | |  | | | || |\  || |/  /  ");
+            Console.ResetColor();
+            System.Threading.Thread.Sleep(500);
+            Console.ForegroundColor = TitleColorRandomizer();
             Console.WriteLine(@"                      \____./  \_/  \_| |_/\_| \_/|___./   ");
             Console.WriteLine("                                                            ");
+            Console.ResetColor();
+            System.Threading.Thread.Sleep(500);
+            Console.ForegroundColor = TitleColorRandomizer();
+            Console.WriteLine("                                         an Erik White joint  ");
             Console.WriteLine("  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             Console.ResetColor();
-            Console.WriteLine("         Press any key to continue.");
-            Console.ReadKey();
+            System.Threading.Thread.Sleep(1500);
+            Console.WriteLine("\n         Press any key to continue.");
+            userInput = Console.ReadKey(true).KeyChar.ToString();
 
 
         }
@@ -45,7 +69,11 @@ namespace LemonadeFoSale
         static public ConsoleColor TitleColorRandomizer()
         {
             Random rndColor = new Random();
-            return titleColors[rndColor.Next(7)];
+            return titleColors[rndColor.Next(0,6)];
+        }
+        static public void MainMenuDisplay()
+        {
+
         }
     }
 }
